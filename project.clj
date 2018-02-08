9ae47da5f7bb5de4c079c88d33311b43b6f98c6c
Daniel Berg <mail@roosta.sh>
2018-02-08 17:20:20 +0100

    Rename project to tonic

--- project.clj
+++ project.clj
@@ -1,16 +1,14 @@
-(defproject flora-ui "0.1.0-SNAPSHOT"
-  :description "Opinionated UI framework built with garden"
-  :url "https://github.com/roosta/flora-ui"
+(defproject tonic "0.1.0-SNAPSHOT"
+  :description "Various frontend tools to ease development"
+  :url "https://github.com/roosta/tonic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [cljs-css-modules "0.2.1" :exclusions [garden]]
                  [herb "0.2.0-SNAPSHOT"]
                  [garden "1.3.3"]
                  [re-frame "0.10.4"]
-                 ;; [cljsjs/hammer "2.0.8-0"]
                  [facjure/mesh "0.4.0"]
                  [rm-hull/inkspot "0.2.1"]]
 
