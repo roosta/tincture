bfca5c9121044a922640bab1a3c397059b72ac09
Daniel Berg <mail@roosta.sh>
2018-02-12 12:35:05 +0100

    Finish container media queries

--- src/tonic/container.cljs
+++ src/tonic/container.cljs
@@ -1,14 +1,30 @@
 (ns tonic.container
   (:require [herb.core :refer-macros [with-style]]
+            [garden.units :refer [em rem]]
             [reagent.core :as r]))
 
 (defn container-style
   []
-  {:width "76rem"
+  ^{:media {{:screen :only :min-width (em 48)} {:width (rem 49)}
+            {:screen :only :min-width (em 64)} {:width (rem 65)}
+            {:screen :only :min-width (em 75)} {:width (rem 76)}}}
+  {:margin-right "auto"
+   :margin-left "auto"})
+
+(defn container-fluid-style
+  []
+  {:padding-right (rem 2)
+   :padding-left (rem 2)
    :margin-right "auto"
    :margin-left "auto"})
 
 (defn container
   []
   (into [:div {:class (with-style container-style)}]
+        (r/children (r/current-component))))
+
+
+(defn container-fluid
+  []
+  (into [:div {:class (with-style container-fluid-style)}]
         (r/children (r/current-component))))
