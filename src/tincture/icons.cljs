ff34801b71ab0c97acad5551968daf27d017451a
Daniel Berg <mail@roosta.sh>
2018-03-19 11:01:49 +0100

    Add chevron up and down

--- src/tincture/icons.cljs
+++ src/tincture/icons.cljs
@@ -65,6 +65,18 @@
              :viewbox "0 0 24 24"}
    [:path {:d "M15.422 16.078l-1.406 1.406-6-6 6-6 1.406 1.406-4.594 4.594z"}]])
 
+(defn chevron-down
+  [{:keys [class]}]
+  [svg-icon {:class class
+             :viewbox "0 0 24 24"}
+   [:path {:d "M 7.41,8.295 6,9.705 l 6,6 6,-6 -1.41,-1.41 -4.59,4.58 z"}]])
+
+(defn chevron-up
+  [{:keys [class]}]
+  [svg-icon {:class class
+             :viewbox "0 0 24 24"}
+   [:path {:d "m16.59 15.705 1.41-1.41-6-6-6 6 1.41 1.41 4.59-4.58z"}]])
+
 (defn menu
   [{:keys [class]}]
   [svg-icon {:class class
