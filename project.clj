13ea12a3f8c67813d45ad1dc40127f037c6797df
Daniel Berg <mail@roosta.sh>
2018-04-03 10:31:11 +0200

    Update herb to 0.5.0-SNAPSHOT, bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tincture "0.1.4-SNAPSHOT"
+(defproject tincture "0.1.5-SNAPSHOT"
   :description "Clojurescript frontend toolkit"
   :url "https://github.com/roosta/tincture"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [cljsjs/react-transition-group "2.2.1-1"]
-                 [herb "0.4.0-SNAPSHOT"]
+                 [herb "0.5.0-SNAPSHOT"]
                  [prismatic/dommy "1.1.0"]
                  [re-frame "0.10.5"]]
 
