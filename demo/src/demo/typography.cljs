efb5cfca6f768be5cdd9ba5cc79aec2caf052687
Daniel Berg <mail@roosta.sh>
2018-05-08 21:55:20 +0200

    Update typography demo

--- demo/src/demo/typography.cljs
+++ demo/src/demo/typography.cljs
@@ -2,15 +2,17 @@
   (:require [reagent.core :as r]
             [garden.units :refer [px]]
             [tincture.typography :refer [typography]]
-            [reagent.debug :as d]
+            [tincture.container :refer [container]]
+            [clojure.string :as str]
+            [reagent.debug :refer [log]]
             [herb.core :refer-macros [<class]]))
 
 (defn main
   []
-  [:div [typography {:kind :headline}
-         "Typography demo"]
-   [typography {:kind :body1}
-    "Hello demo"]
-
-   [:div [:a {:href "/"} "go to the home page"]]
-   ])
+  [container
+   (for [variant
+         [:display4 :display3 :display2 :display1 :headline :title :subheading
+         :button :body1 :body2]]
+     ^{:key variant}
+     [typography {:variant variant}
+      variant])])
