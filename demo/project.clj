65d1e3a5034c46ad8c98f744aec62d55090f8722
Daniel Berg <mail@roosta.sh>
2018-05-21 20:57:28 +0200

    Add re-frame-10x, orchestra. Format file

--- demo/project.clj
+++ demo/project.clj
@@ -2,16 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [secretary "1.2.3"]
                  [venantius/accountant "0.2.4"]
                  [org.clojure/clojurescript "1.10.238"]
                  [herb "0.5.0-SNAPSHOT"]
-
                  [prismatic/dommy "1.1.0"]
-
                  [cljsjs/react-transition-group "2.3.1-0"]
                  [figwheel-sidecar "0.5.16"]
                  [re-frame "0.10.5"]
@@ -32,28 +30,30 @@
   :source-paths ["src" "../src"]
 
   :figwheel {:http-server-root "."
-             :nrepl-port 7899
-             :server-port 3460
+             :nrepl-port       7899
+             :server-port      3460
              :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
-             :css-dirs ["public/css"]}
+             :css-dirs         ["public/css"]}
 
   :cljsbuild {:builds {:app
                        {:source-paths ["src" "../src"]
                         :compiler
-                        {:main "demo.core"
-                         :output-to "public/js/app.js"
-                         :output-dir "public/js/out"
-                         :asset-path   "js/out"
-                         :preloads [re-frisk.preload]
-                         :source-map true
-                         :optimizations :none
-                         :pretty-print  true}
+                        {:main            "demo.core"
+                         :output-to       "public/js/app.js"
+                         :output-dir      "public/js/out"
+                         :asset-path      "js/out"
+                         :preloads        [day8.re-frame-10x.preload]
+                         :closure-defines {"re_frame.trace.trace_enabled_QMARK_" true}
+                         :source-map      true
+                         :optimizations   :none
+                         :pretty-print    true}
                         :figwheel
                         {:on-jsload "demo.core/mount-root" }}}}
 
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [figwheel-sidecar "0.5.16"]
                                   [org.clojure/tools.nrepl "0.2.13"]
-                                  [re-frisk "0.5.4"]
+                                  [day8.re-frame/re-frame-10x "0.3.3-react16"]
+                                  [orchestra "2017.11.12-1"]
                                   [cider/piggieback "0.3.5"]]
                    :source-paths ["script"]}})
