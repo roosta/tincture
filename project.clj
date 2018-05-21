28c37b761bdb65c3641fad3005f2d50173f7ecd5
Daniel Berg <mail@roosta.sh>
2018-05-21 20:57:01 +0200

    Remove unneeded cljs build

--- project.clj
+++ project.clj
@@ -10,18 +10,4 @@
                  [herb "0.5.0-SNAPSHOT"]
                  [prismatic/dommy "1.1.0"]
                  [re-frame "0.10.5"]]
-
-  :source-paths ["src"]
-
-  :cljsbuild {:builds [{:id "test"
-                        :source-paths ["src" "test"]
-                        :compiler {:output-to "target/cljs/test/test.js"
-                                   :output-dir "target/cljs/test"
-                                   :optimizations :none
-                                   :pretty-print true
-                                   :source-map true
-                                   :main tincture.runner}}
-                       {:id "prod"
-                        :source-paths ["src"]
-                        :compiler {:output-to "tincture.js"
-                                   :optimizations :advanced}}]})
+  :source-paths ["src"])
