e8f000c1ab631cd34405699c0591421f81a2938a
Daniel Berg <mail@roosta.sh>
2018-05-21 21:18:16 +0200

    Update project description

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject tincture "0.1.5-SNAPSHOT"
-  :description "Clojurescript frontend toolkit"
+  :description "Frontend development toolkit"
   :url "https://github.com/roosta/tincture"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [cljsjs/react-transition-group "2.3.1-0"]
