ce6fa54c0f68646f32ca2c6ec801ec4ec9573806
Daniel Berg <mail@roosta.sh>
2018-05-22 21:04:15 +0200

    Condense gradient fns, and rename to css

--- src/tincture/gradient.cljs
+++ src/tincture/gradient.cljs
@@ -6,42 +6,32 @@
   (:require-macros
    [tincture.macros :as macros]))
 
-(def gradients (macros/ui-gradients "gradients.json"))
+(def collection (macros/ui-gradients "gradients.json"))
 
-(defn- css-gradient
-  "Takes a direction and a vector of colors and returns a set with CSS strings"
-  [direction colors]
-  (let [gradient-str (str "linear-gradient(to " (name direction) ", " (str/join ", " colors) ")")]
-    #{(first colors)
-      (str "-webkit-" gradient-str)
-      gradient-str}))
-
-(defn gradient
+(defn css
   "Takes a name, that should correspond with gradients listed at uigradients.com
   and can be either string or keyword, and a direction: #{:left :right :up :down}
   Returns a set of CSS strings used with a :background property passed to
   Garden.
 
   Example
-  `(garden.core/css [:h2 {:background (gradent :vice-city :left)}])`"
+  `(garden.core/css [:h2 {:background (gradient/css :vice-city :left)}])`"
   [name direction]
   (let [kw (utils/name->kword name)
-        colors (kw gradients)]
-    (css-gradient direction colors)))
+        colors (kw collection)
+        gradient-str (str "linear-gradient(to " (name direction) ", " (str/join ", " colors) ")")]
+    #{(first colors)
+      (str "-webkit-" gradient-str)
+      gradient-str}))
 
-(s/def ::ui-gradient-keyword (->> gradients (map key) set))
+(s/def ::ui-gradient-keyword (->> collection (map key) set))
 (s/def ::directions #{:left :right :up :down})
 (s/def ::hex-color (s/and string? #(re-matches #"^#(?:[0-9a-fA-F]{3}){1,2}$" %)))
-(s/def ::css-set (s/coll-of string? :kind set? :min-count 3))
 
-(s/fdef gradient
+(s/fdef css
   :args (s/cat :name (s/or :keyword ::ui-gradient-keyword
                            :name (s/and string? #(s/valid? ::ui-gradient-keyword (utils/name->kword %))))
                :direction ::directions)
-  :ret ::css-set)
+  :ret (s/coll-of string? :kind set? :min-count 3))
 
-(s/fdef css-gradient
-  :args (s/cat :direction ::directions
-               :colors (s/coll-of ::hex-color :kind vector? :min-count 2 :distinct true))
-  :ret ::css-set)
 
