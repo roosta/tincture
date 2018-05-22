4855072aad28c26a8a5bf7c19146739c4ada6d02
Daniel Berg <mail@roosta.sh>
2018-05-22 22:04:06 +0200

    Fix name issue, change css fn to multi arity

--- src/tincture/gradient.cljs
+++ src/tincture/gradient.cljs
@@ -2,6 +2,7 @@
   (:require
    [cljs.spec.alpha :as s :include-macros true]
    [clojure.string :as str]
+   [reagent.debug :refer [log]]
    [tincture.utils :as utils])
   (:require-macros
    [tincture.macros :as macros]))
@@ -12,26 +13,30 @@
   "Takes a name, that should correspond with gradients listed at uigradients.com
   and can be either string or keyword, and a direction: #{:left :right :up :down}
   Returns a set of CSS strings used with a :background property passed to
-  Garden.
+  Garden. The return value contains browser fallbacks.
 
   Example
   `(garden.core/css [:h2 {:background (gradient/css :vice-city :left)}])`"
-  [name direction]
-  (let [kw (utils/name->kword name)
-        colors (kw collection)
-        gradient-str (str "linear-gradient(to " (name direction) ", " (str/join ", " colors) ")")]
-    #{(first colors)
-      (str "-webkit-" gradient-str)
-      gradient-str}))
+  ([palette-name]
+   (css palette-name :left))
+  ([palette-name direction]
+   (let [kw (utils/name->kword palette-name)
+         colors (kw collection)
+         gradient-str (str "linear-gradient(to " (name direction) ", " (str/join ", " colors) ")")]
+     #{(first colors)
+       (str "-webkit-" gradient-str)
+       gradient-str})))
 
 (s/def ::ui-gradient-keyword (->> collection (map key) set))
 (s/def ::directions #{:left :right :up :down})
 (s/def ::hex-color (s/and string? #(re-matches #"^#(?:[0-9a-fA-F]{3}){1,2}$" %)))
+(s/def ::palette-name (s/or :keyword ::ui-gradient-keyword
+                            :palette-name (s/and string? #(s/valid? ::ui-gradient-keyword (utils/name->kword %)))))
 
 (s/fdef css
-  :args (s/cat :name (s/or :keyword ::ui-gradient-keyword
-                           :name (s/and string? #(s/valid? ::ui-gradient-keyword (utils/name->kword %))))
-               :direction ::directions)
+  :args (s/alt :single-arg ::palette-name
+               :double-arg (s/cat :name ::palette-name
+                                  :direction ::directions))
   :ret (s/coll-of string? :kind set? :min-count 3))
 
 
