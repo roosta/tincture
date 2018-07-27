77971ad786474653cf1e0eb8a9099af0795b8f40
Daniel Berg <mail@roosta.sh>
2018-07-27 10:04:41 +0200

    Bump to version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tincture "0.1.5"
+(defproject tincture "0.1.6"
   :description "Frontend development toolkit"
   :url "https://github.com/roosta/tincture"
   :license {:name "Eclipse Public License"
