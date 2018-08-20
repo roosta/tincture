32b64309decc5d0779c1ed100b70f97feefb7aaf
Daniel Berg <mail@roosta.sh>
2018-08-20 13:54:57 +0200

    Change clojure/script scope to provided

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/roosta/tincture"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.339" :scope "provided"]
                  [cljsjs/react-transition-group "2.4.0-0"]
                  [herb "0.5.1-SNAPSHOT"]
                  [prismatic/dommy "1.1.0"]
