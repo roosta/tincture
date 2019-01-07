b94f90c7417e74014c61499938a32c72eb3856f8
Daniel Berg <mail@roosta.sh>
2019-01-07 12:46:42 +0100

    Bump herb

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.339" :scope "provided"]
                  [cljsjs/react-transition-group "2.4.0-0"]
-                 [herb "0.5.1-SNAPSHOT"]
+                 [herb "0.7.3-SNAPSHOT"]
                  [prismatic/dommy "1.1.0"]
                  [re-frame "0.10.5"]]
 
