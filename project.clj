be00dabe54a1942e6120de4e70d3d66de478f519
Daniel Berg <mail@roosta.sh>
2019-01-28 15:44:31 +0100

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/roosta/tincture"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.339" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.439" :scope "provided"]
                  [cljsjs/react-transition-group "2.4.0-0"]
                  [herb "0.7.3-SNAPSHOT"]
                  [prismatic/dommy "1.1.0"]
-                 [re-frame "0.10.5"]]
+                 [re-frame "0.10.6"]]
 
   :cljsbuild {:builds [{:id "prod"
                         :source-paths ["src"]
