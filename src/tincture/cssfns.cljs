c2519edc4b6ee824fd40f69233b2fea7378f8002
Daniel Berg <mail@roosta.sh>
2019-01-30 19:17:50 +0100

    Add cssfns namespace and populate with some common fns

---
+++ src/tincture/cssfns.cljs
@@ -0,0 +1,12 @@
+(ns tincture.cssfns
+  (:require-macros [garden.def :refer [defcssfn]]))
+
+(defcssfn linear-gradient
+  [dir c1 p1 c2 p2]
+  [dir [c1 p1] [c2 p2]])
+
+(defcssfn rgb
+  ([c1 c2 c3] [c1 c2 c3])
+  ([c1 c2 c3 a] [c1 c2 c3 a]))
+
+(defcssfn url)
