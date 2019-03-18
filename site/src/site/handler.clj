e9195cb6a7c2c8273e880cd2169abc256ad6632a
Daniel Berg <mail@roosta.sh>
2019-03-18 18:47:47 +0100

    Use catch-all route

--- site/src/site/handler.clj
+++ site/src/site/handler.clj
@@ -30,12 +30,7 @@
 (def app
   (reitit-ring/ring-handler
    (reitit-ring/router
-    [["/" {:get {:handler index-handler}}]
-     ["/typography" {:get {:handler index-handler}}]
-     ["/slide" {:get {:handler index-handler}}]
-     ["/gradient" {:get {:handler index-handler}}]
-     ["/paper" {:get {:handler index-handler}}]
-     ["/grid" {:get {:handler index-handler}}]]
+    [["/*" {:get {:handler index-handler}}]]
     {:data {:middleware middleware}})
    (reitit-ring/routes
     (reitit-ring/create-resource-handler {:path "/" :root "/public"})
