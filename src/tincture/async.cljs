5c53f89fa772d7359b1191985200db53ee786b25
Daniel Berg <mail@roosta.sh>
2019-03-23 16:03:00 +0100

    Add docstrings

--- src/tincture/async.cljs
+++ src/tincture/async.cljs
@@ -1,14 +1,42 @@
 (ns tincture.async
-  "https://medium.com/@alehatsman/clojurescript-throttle-debounce-a651dfb66ac"
+  "Adaptions of google closure library async Throttle and Debounce.
+
+  Source: https://medium.com/@alehatsman/clojurescript-throttle-debounce-a651dfb66ac"
+
   (:import [goog.async Throttle Debouncer]))
 
-(defn disposable->function [disposable listener interval]
+(defn- disposable->function [disposable listener interval]
   (let [disposable-instance (disposable. listener interval)]
     (fn [& args]
       (.apply (.-fire disposable-instance) disposable-instance (to-array args)))))
 
-(defn throttle [listener interval]
+(defn throttle
+  "Takes a listener function and interval and creates an instance of
+  `goog.async.Throttle`.
+  Example usage:
+  ```clojure
+  (require '[goog.events :as gevents])
+  (gevents/listen
+   js/document
+   \"scroll\"
+   (throttle on-scroll 200))
+  ```
+  "
+  [listener interval]
   (disposable->function Throttle listener interval))
 
-(defn debounce [listener interval]
+(defn debounce
+  "Takes a listener function and interval and creates an instance of
+  `goog.async.Debounce`
+  Example usage:
+  ```clojure
+  (require '[goog.events :as gevents])
+  (gevents/listen
+   js/window
+   \"resize\"
+   (debounce on-resize 200))
+  ```
+  "
+
+  [listener interval]
   (disposable->function Debouncer listener interval))
