9dfc60f59e86e6d8226e316e439a1d03e07ed0be
Daniel Berg <mail@roosta.sh>
2019-03-30 15:37:08 +0100

    Add color prop

--- src/tincture/typography.cljs
+++ src/tincture/typography.cljs
@@ -20,92 +20,105 @@
                             :regular 400
                             :medium 500})
 
+(def ^:private colors {:light (rgb 0 0 0 0.87)
+                       :dark "#fff"
+                       :secondary-light (rgb 0 0 0 0.54)
+                       :secondary-dark (rgb 255 255 255 0.7)})
+
 (s/def ::valid-variants
   #{:h1 :h2 :h3 :h4 :h5 :h6 :subtitle1 :subtitle2 :body1 :body2 :caption :button
    :overline :sr-only :inherit})
 
+(s/def ::valid-colors #{:light :dark :secondary-light :secondary-dark})
+
 (defn- variants
-  [variant]
-  (let [font-families @(rf/subscribe [:tincture/font-families])]
-    (variant {:h1 {:color (rgb 0 0 0 0.87)
+  [variant color]
+  (let [color (get colors color)
+        font-families @(rf/subscribe [:tincture/font-families])]
+    (variant {:h1 {:color color
                    :font-family (:headline font-families)
                    :font-size (rem 6)
                    :font-weight (:light font-weight)
                    :line-height 1}
 
-              :h2 {:color (rgb 0 0 0 0.87)
+              :h2 {:color color
                    :font-family (:headline font-families)
                    :font-size (rem 3.75)
                    :font-weight (:light font-weight),
                    :line-height 1}
 
-              :h3 {:color (rgb 0 0 0 0.87)
+              :h3 {:color color
                    :font-family (:headline font-families)
                    :font-size (rem 3)
                    :font-weight (:regular font-weight)
                    :line-height 1.04}
 
-              :h4 {:color (rgb 0 0 0 0.87)
+              :h4 {:color color
                    :font-family (:headline font-families)
                    :font-size (rem 2.125)
                    :font-weight (:regular font-weight)
                    :line-height 1.17}
 
-              :h5 {:color (rgb 0 0 0 0.87)
+              :h5 {:color color
                    :font-family (:headline font-families)
                    :font-size (rem 1.5)
                    :font-weight (:regular font-weight)
                    :line-height 1.33}
 
-              :h6 {:color (rgb 0 0 0 0.87)
+              :h6 {:color color
                    :font-family (:headline font-families)
                    :font-size (rem 1.25)
                    :font-weight (:medium font-weight)
                    :line-height 1.6}
 
-              :subtitle1 {:color (rgb 0 0 0 0.87)
+              :subtitle1 {:color color
                           :font-family (:headline font-families)
                           :font-size (rem 1)
                           :font-weight (:regular font-weight)
                           :line-height 1.75}
 
-              :subtitle2 {:color (rgb 0 0 0 0.87)
+              :subtitle2 {:color color
                           :font-family (:headline font-families)
                           :font-size (rem 0.875)
                           :font-weight (:medium font-weight)
                           :line-height 1.57}
 
-              :body1 {:color (rgb 0 0 0 0.87)
+              :body1 {:color color
                       :font-family (:body font-families)
                       :font-size (rem 1)
                       :font-weight (:regular font-weight)
                       :line-height 1.5}
 
-              :body2 {:color (rgb 0 0 0 0.87)
+              :body2 {:color color
                       :font-family (:body font-families)
                       :font-size (rem 0.875)
                       :font-weight (:regular font-weight)
                       :line-height 1.5}
 
-              :caption {:color (rgb 0 0 0 0.87)
+              :caption {:color color
                         :font-family (:body font-families)
                         :font-size (rem 0.75)
                         :font-weight (:regular font-weight)
                         :line-height 1.66}
 
-              :button {:color (rgb 0 0 0 0.87)
+              :button {:color color
                        :font-family (:body font-families)
                        :font-size (rem 0.875)
                        :font-weight (:medium font-weight)
                        :text-transform :uppercase
                        :line-height 1.75}
 
-              :overline {:color (rgb 0 0 0 0.87)
+              :overline {:color color
                          :font-family (:body font-families)
                          :font-size (rem 0.75)
                          :font-weight (:regular font-weight)
                          :text-transform :uppercase
-                         :line-height 2.66}})))
+                         :line-height 2.66}
+
+              :sr-only {:position "absolute"
+                        :height (px 1)
+                        :width (px 1)
+                        :overflow "hidden"}})))
 
 
 (def ^:private mapping
@@ -130,9 +143,10 @@
 (s/def ::gutter-bottom boolean?)
 
 (defn- typography-style
-  [variant align font-style direction elevation gutter-bottom paragraph]
+  [variant align font-style direction elevation gutter-bottom paragraph color]
   (let [k (str/join "-" [(name variant) (name align) (name font-style)
-                         (name direction) elevation gutter-bottom paragraph])
+                         (name direction) elevation gutter-bottom paragraph
+                         (name color)])
 
         root {:margin 0}
 
@@ -154,7 +168,7 @@
       (into {}
             (merge
              {:text-shadow (t/text-shadow elevation)}
-             (variants variant)
+             (variants variant color)
              (when paragraph p-style)
              (when gutter-bottom gutter-bottom-style)
              (direction directions)
@@ -164,14 +178,17 @@
       {:key k})))
 
 (defn typography
-  [{:keys [variant align class elevation font-style on-click direction component gutter-bottom paragraph]
+  [{:keys [variant align class elevation font-style
+           on-click direction component gutter-bottom
+           paragraph color]
     :or {variant :body2
          font-style :normal
          align :left
          direction :ltr
          paragraph false
          gutter-bottom false
-         elevation 0}}]
+         elevation 0
+         color :light}}]
   (let [variant (check-spec variant ::valid-variants)
         align (check-spec align ::valid-aligns)
         class (check-spec class ::class)
@@ -180,9 +197,10 @@
         font-style (check-spec font-style ::valid-font-styles)
         on-click (check-spec on-click ::on-click)
         gutter-bottom (check-spec gutter-bottom ::gutter-bottom)
+        color (check-spec color ::valid-colors)
         paragraph (check-spec paragraph ::paragraph)]
     (into
      [(if paragraph :p (or component (variant mapping) :span))
       {:on-click on-click
-       :class [class (<class typography-style variant align font-style direction elevation gutter-bottom paragraph)]}]
+       :class [class (<class typography-style variant align font-style direction elevation gutter-bottom paragraph color)]}]
      (r/children (r/current-component)))))
