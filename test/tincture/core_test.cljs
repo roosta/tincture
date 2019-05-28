d8a077fb923355639c6e3c676a7c244f1ab07b1a
Daniel Berg <mail@roosta.sh>
2019-05-28 20:20:43 +0200

    Add clamp test

--- test/tincture/core_test.cljs
+++ test/tincture/core_test.cljs
@@ -1,6 +1,9 @@
 (ns tincture.core-test
-  (:require [cljs.test :refer-macros [deftest testing is]]))
+  (:require [cljs.test :refer-macros [deftest testing is]]
+            [tincture.core :as t]))
 
-(deftest fake-test
-  (testing "fake description"
-    (is (= 1 1))))
+(deftest clamp
+  (testing "Clamp function"
+    (is (= (t/clamp 42 10 30) 30))
+    (is (= (t/clamp 10 20 30) 20))
+    (is (= (t/clamp 10 5 15) 10))))
