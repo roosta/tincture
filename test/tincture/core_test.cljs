439a3776365a0d5fb561e4970d941ea53ab46bae
Daniel Berg <mail@roosta.sh>
2019-05-28 20:40:34 +0200

    Add index-of test (failing)

--- test/tincture/core_test.cljs
+++ test/tincture/core_test.cljs
@@ -7,3 +7,10 @@
     (is (= (t/clamp 42 10 30) 30))
     (is (= (t/clamp 10 20 30) 20))
     (is (= (t/clamp 10 5 15) 10))))
+
+(deftest index-of
+  (testing "Get index from random collections using 'index-of'"
+    (is (= (t/index-of [1 2 3 4] 3) 2))
+    (is (= (t/index-of #{:a :b :c} :b) 1))
+    (is (= (t/index-of {:a 1 :b 2 :c 3} :a) 0))
+    ))
