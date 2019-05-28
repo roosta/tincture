e0e6efe9dc0ef384e1efda71737ece30eee38e6d
Daniel Berg <mail@roosta.sh>
2019-05-28 19:51:19 +0200

    Setup chrome test runner

--- project.clj
+++ project.clj
@@ -8,10 +8,16 @@
           :metadata {:doc/format :markdown}
           :output-path "docs"
           :source-paths ["src"]}
+  :profiles {:dev {:dependencies [[clj-chrome-devtools "20190502"]]}}
+  :cljsbuild {:builds [{:id "test"
+                        :source-paths ["src" "test"]
+                        :compiler {:optimizations :whitespace
+                                   :output-to "target/cljs/test/test.js"
+                                   :pretty-print true}}]}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [cljsjs/react-transition-group "2.4.0-0"]
-                 [herb "0.7.3-SNAPSHOT"]
-                 ;; [prismatic/dommy "1.1.0"]
+                 [herb "0.8.1"]
+                 [prismatic/dommy "1.1.0"]
                  [re-frame "0.10.6"]]
   :source-paths ["src"])
---
+++ test/tincture/core_test.cljs
@@ -0,0 +1,6 @@
+(ns tincture.core-test
+  (:require [cljs.test :refer-macros [deftest testing is]]))
+
+(deftest fake-test
+  (testing "fake description"
+    (is (= 1 1))))
