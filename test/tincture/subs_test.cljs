3408f0790a186fe3ddeb67155ef398501fab095f
Daniel Berg <mail@roosta.sh>
2019-06-17 18:18:58 +0200

    Add breakpoint sub tests

--- test/tincture/subs_test.cljs
+++ test/tincture/subs_test.cljs
@@ -26,3 +26,34 @@
       (with-redefs [device/isDesktop (fn [] false)
                     device/isMobile (fn [] true)]
         (is (= @device :mobile))))))
+
+(deftest breakpoints
+  (testing "Breakpoint down and up subs"
+    (rf-test/run-test-sync
+     (let [xs-down (rf/subscribe [:tincture/breakpoint-down :xs])
+           sm-down (rf/subscribe [:tincture/breakpoint-down :sm])
+           md-down (rf/subscribe [:tincture/breakpoint-down :md])
+           lg-down (rf/subscribe [:tincture/breakpoint-down :lg])
+           xl-down (rf/subscribe [:tincture/breakpoint-down :xl])
+
+           xs-up (rf/subscribe [:tincture/breakpoint-up :xs])
+           sm-up (rf/subscribe [:tincture/breakpoint-up :sm])
+           md-up (rf/subscribe [:tincture/breakpoint-up :md])
+           lg-up (rf/subscribe [:tincture/breakpoint-up :lg])
+           xl-up (rf/subscribe [:tincture/breakpoint-up :xl])]
+       (rf/dispatch [:tincture/set-viewport-size [0 0]])
+       (is @xs-down)
+       (rf/dispatch [:tincture/set-viewport-size [1 0]])
+       (is @xs-up)
+       (is @sm-down)
+       (rf/dispatch [:tincture/set-viewport-size [650 0]])
+       (is @sm-up)
+       (is @md-down)
+       (rf/dispatch [:tincture/set-viewport-size [1000 0]])
+       (is @md-up)
+       (is @lg-down)
+       (rf/dispatch [:tincture/set-viewport-size [1300 0]])
+       (is @lg-up)
+       (is @xl-down)
+       (rf/dispatch [:tincture/set-viewport-size [2000 0]])
+       (is @xl-up)))))
