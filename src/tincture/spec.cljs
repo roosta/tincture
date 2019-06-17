16e560e9d7dfacf8acc984cef0c1086bf651db9e
Daniel Berg <mail@roosta.sh>
2019-06-17 18:40:55 +0200

    Remove trailing space

--- src/tincture/spec.cljs
+++ src/tincture/spec.cljs
@@ -6,7 +6,7 @@
   the spec, otherwise return passed value"
   [value spec]
   (if-not (s/valid? spec value)
-    (throw (ex-info "Invalid value " (s/explain-data spec value)))
+    (throw (ex-info "Invalid value" (s/explain-data spec value)))
     value))
 
 (def email-re #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
