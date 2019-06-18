d051da194073d7c486f4e1131b095e9df5fc8bb8
Daniel Berg <mail@roosta.sh>
2019-06-18 19:37:26 +0200

    Use check-spec in gradient/css
    fdef requires instrumentation being enabled, no guarantee that
    consumer have this enabled.

--- src/tincture/gradient.cljs
+++ src/tincture/gradient.cljs
@@ -2,12 +2,19 @@
   (:require
    [cljs.spec.alpha :as s :include-macros true]
    [clojure.string :as str]
+   [tincture.spec :refer [check-spec]]
    [reagent.debug :refer [log]]
    [tincture.utils :as utils])
   (:require-macros
    [tincture.macros :as macros]))
 
 (def ^:private collection (macros/ui-gradients "gradients.json"))
+
+(s/def ::ui-gradient-keyword (->> collection (map key) set))
+(s/def ::direction #{:left :right :up :down})
+(s/def ::hex-color (s/and string? #(re-matches #"^#(?:[0-9a-fA-F]{3}){1,2}$" %)))
+(s/def ::palette-name (s/or :keyword ::ui-gradient-keyword
+                            :palette-name (s/and string? #(s/valid? ::ui-gradient-keyword (utils/name->kword %)))))
 
 (defn css
   "Takes a name, that should correspond with gradients listed
@@ -25,21 +32,17 @@
   ([palette-name]
    (css palette-name :left))
   ([palette-name direction]
-   (let [kw (utils/name->kword palette-name)
+   (let [palette-name (check-spec palette-name ::palette-name) 
+         direction (check-spec direction ::direction)
+         kw (utils/name->kword palette-name)
          colors (kw collection)
          gradient-str (str "linear-gradient(to " (name direction) ", " (str/join ", " colors) ")")]
      #{(first colors)
        (str "-webkit-" gradient-str)
        gradient-str})))
 
-(s/def ::ui-gradient-keyword (->> collection (map key) set))
-(s/def ::directions #{:left :right :up :down})
-(s/def ::hex-color (s/and string? #(re-matches #"^#(?:[0-9a-fA-F]{3}){1,2}$" %)))
-(s/def ::palette-name (s/or :keyword ::ui-gradient-keyword
-                            :palette-name (s/and string? #(s/valid? ::ui-gradient-keyword (utils/name->kword %)))))
-
-(s/fdef css
+#_(s/fdef css
   :args (s/alt :single-arg ::palette-name
                :double-arg (s/cat :name ::palette-name
-                                  :direction ::directions))
+                                  :direction ::direction))
   :ret (s/coll-of string? :kind set? :min-count 3))
