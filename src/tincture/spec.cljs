4138bf88a79b5e3f46396493de7ef3f283844259
Daniel Berg <mail@roosta.sh>
2019-06-20 19:32:14 +0200

    Add message as an optional argument for check-spec

--- src/tincture/spec.cljs
+++ src/tincture/spec.cljs
@@ -4,10 +4,12 @@
 (defn check-spec
   "Takes a value and a key for a spec, throw an exception if value doesn't match
   the spec, otherwise return passed value"
-  [value spec]
-  (if-not (s/valid? spec value)
-    (throw (ex-info "Invalid value" (s/explain-data spec value)))
-    value))
+  ([value spec]
+   (check-spec value spec "Invalid value"))
+  ([value spec message]
+   (if-not (s/valid? spec value)
+     (throw (ex-info message (s/explain-data spec value)))
+     value)))
 
 (def email-re #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
 (s/def ::email #(re-matches email-re %))
