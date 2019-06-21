a42f905a4b6ce46b6c6ed64b1c1e6b27aa020ffd
Daniel Berg <mail@roosta.sh>
2019-06-21 21:26:05 +0200

    Change breakpoints for grid container

--- site/src/site/typography.cljs
+++ site/src/site/typography.cljs
@@ -54,7 +54,8 @@
     [Grid {:item true
            :component Paper
            :class (<class styles :grid)
-           :xs 10}
+           :md 10
+           :xs 12}
      (for [variant
            [:h1 :h2 :h3 :h4 :h5 :h6 :subtitle1 :subtitle2 :body1 :body2 :button
             :caption :overline]]
