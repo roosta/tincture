5d68539dac6031822969f25dd9fce134ff6ff7e9
Daniel Berg <mail@roosta.sh>
2019-06-21 15:19:54 +0200

    Add typography style tests

--- test/tincture/typography_test.cljs
+++ test/tincture/typography_test.cljs
@@ -44,6 +44,32 @@
 
 (deftest typography-style
   (testing "typography style function"
-    (let [f #'tincture.typography/typography-style]
-      ))
-  )
+    (rf-test/run-test-sync
+     (core/init!)
+       (let [style (#'tincture.typography/typography-style
+                    :body2 :left :normal :ltr 0 false false :light false)]
+         (is (= (:line-height style) 1.5))
+         (is (= (-> style :color :f) "rgb"))
+         (is (= (-> style :color :args) [0 0 0 0.87]))
+         (is (= (:text-align style) "left"))
+         (is (= (-> style :font-size :unit) :rem))
+         (is (= (-> style :font-size :magnitude) 0.875))
+         (is (= (:text-shadow style) "none"))
+         (is (= (:direction style) "ltr"))
+         (is (= (:font-family style) ["'Raleway'" "'Helvetica Neue'" "Arial" "Helvetica" "sans-serif"]))
+         (is (= (-> style meta :key) "body2-left-normal-ltr-0-false-false-light-false")))
+
+       (let [style (#'tincture.typography/typography-style
+                    :h3 :right :italic :rtl 3 true true :dark true)]
+         (is (= (:line-height style) 1.04))
+         (is (= (:text-overflow style) "ellipsis"))
+         (is (= (:color style) "#fff"))
+         (is (= (:text-align style) "right"))
+         (is (= (:white-space style) "nowrap"))
+         (is (= (:overflow style) :hidden))
+         (is (= (:text-shadow style) "4px 4px 8px rgba(0, 0, 0, 0.3)"))
+         (is (= (-> style :margin-bottom :unit) :em))
+         (is (= (-> style :margin-bottom :magnitude) 0.35))
+         (is (= (:direction style) "rtl"))
+         (is (= (:margin style) 0))
+         (is (= (-> style meta :key) "h3-right-italic-rtl-3-true-true-dark-true"))))))
