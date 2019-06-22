c0cf22b4082da39872635bcc50d994a616a94e96
Daniel Berg <mail@roosta.sh>
2019-06-22 13:16:23 +0200

    Change gradient demo to use Grid component

--- site/src/site/gradient.cljs
+++ site/src/site/gradient.cljs
@@ -4,6 +4,7 @@
    [tincture.typography :refer [typography]]
    [tincture.cssfns :refer [rgb]]
    [herb.core :refer [<class defgroup]]
+   [tincture.grid :refer [Grid]]
    [reagent.debug :refer [log]]
    [garden.units :refer [px]]
    [tincture.gradient :as gradient]))
@@ -11,18 +12,12 @@
 (defn gradient-box
   [gradient kw]
   ^{:key kw}
-  {:display "flex"
-   :align-items "center"
-   :justify-content "center"
-   :background gradient
-   :height (px 200)})
+  {:background gradient
+   :height (px 200)
+   })
 
 (defgroup styles
-  {:grid {:display "grid"
-          :row-gap "10px"
-          :column-gap "10px"
-          :grid-template-columns "repeat(4, 1fr)"}
-   :headline {}
+  {:headline {}
    :container {:padding (px 16)}
    :palette-name {:background (rgb 0 0 0 0.53)
                   :width "100%"}
@@ -40,14 +35,19 @@
                 :align :center
                 :class (<class styles :text)}
     "Tincture provides a gradient tool, it uses gradients defined at " [:a {:href "https://uigradients.com"} "uigradients.com"]]
-   [:div {:class (<class styles :grid)}
+   [Grid {:container true
+          :spacing 16}
     (for [g (deref #'tincture.gradient/collection)]
       (let [kw (key g)]
         ^{:key kw}
-        [:div {:class (<class gradient-box (gradient/css (key g)) kw)}
-         [typography {:class (<class styles :palette-name)
-                      :align :center
-                      :color :dark
-                      :variant :subtitle1}
-          (name kw)]
+        [Grid {:item true
+               :md 3}
+         [Grid {:container true
+                :align-items :center
+                :class (<class gradient-box (gradient/css (key g)) kw)}
+           [typography {:class (<class styles :palette-name)
+                        :align :center
+                        :color :dark
+                        :variant :subtitle1}
+            (name kw)]]
          ]))]])
