14b3eb1866c8d99d001264b3e397fe518fcbc336
Daniel Berg <mail@roosta.sh>
2019-07-04 16:45:50 +0200

    Add docstring to rgb cssfn

--- src/tincture/cssfns.cljs
+++ src/tincture/cssfns.cljs
@@ -5,7 +5,17 @@
   [dir color1 position1 color2 position2]
   [dir [color1 position1] [color2 position2]])
 
-(defcssfn rgb
+(defcssfn
+  ^{:doc "Create a RGB(A) color string by passing red green blue and optionally alpha
+
+Example usage:
+```clojure
+(garden.core/css [:.my-class {:color (rgb 255 255 255 0.2)}])
+
+;;=> \".my-class {color: rgb(255, 255, 255, 0.2);}\"
+```
+"}
+  rgb
   ([r g b] [r g b])
   ([r g b a] [r g b a]))
 
