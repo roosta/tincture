18f335ffbbf8966ed336cf08fbf3ac6168478e14
Daniel Berg <mail@roosta.sh>
2019-07-05 17:03:01 +0200

    Spec check rgb arguments

--- src/tincture/cssfns.cljs
+++ src/tincture/cssfns.cljs
@@ -1,9 +1,21 @@
 (ns tincture.cssfns
-  (:require-macros [garden.def :refer [defcssfn]]))
+  (:require-macros [garden.def :refer [defcssfn]])
+  (:require
+   [tincture.spec :refer [check-spec]]
+   [cljs.spec.alpha :as s :include-macros true]))
 
 (defcssfn linear-gradient
   [dir color1 position1 color2 position2]
   [dir [color1 position1] [color2 position2]])
+
+(s/def ::percent
+  (letfn [(pred [s]
+            (re-matches #"^(\d\d?|100?)%$" (str s)))]
+    (s/spec pred)))
+
+(s/def ::color (s/or :percent ::percent :int (s/int-in 0 256)))
+(def color-msg "Invalid color passed to rgb cssfn, 0-255 or percent 0%-100%")
+(def alpha-msg "Invalid alpha value")
 
 (defcssfn
   ^{:doc "Create a RGB(A) color string by passing red green blue and optionally alpha
@@ -16,8 +28,16 @@ Example usage:
 ```
 "}
   rgb
-  ([r g b] [r g b])
-  ([r g b a] [r g b a]))
+  ([r g b]
+   (let [r (check-spec r ::color color-msg)
+         g (check-spec g ::color color-msg)
+         b (check-spec b ::color color-msg)]
+     [r g b]))
+  ([r g b a]
+   (let [r (check-spec r ::color color-msg)
+         g (check-spec g ::color color-msg)
+         b (check-spec b ::color color-msg)]
+     [r g b a])))
 
 (defcssfn url)
 
