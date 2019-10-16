4360f3e0d962adc6ded8e71a9d89bcc675bc70ab
Daniel Berg <mail@roosta.sh>
2019-10-16 12:45:58 +0200

    Upgrade site deps

--- site/project.clj
+++ site/project.clj
@@ -4,20 +4,20 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring-server "0.5.0"]
                  [reagent "0.8.1"]
                  [tincture "0.3.3-SNAPSHOT"]
-                 [reagent-utils "0.3.2"]
+                 [reagent-utils "0.3.3"]
                  [ring "1.7.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [re-frame "0.10.6"]
+                 [re-frame "0.10.9"]
                  [hiccup "1.0.5"]
                  [cljsjs/hammer "2.0.8-0"]
-                 [yogthos/config "1.1.1"]
+                 [yogthos/config "1.1.6"]
                  [org.clojure/clojurescript "1.10.520"
                   :scope "provided"]
-                 [metosin/reitit "0.3.1"]
+                 [metosin/reitit "0.3.10"]
                  [pez/clerk "1.0.0"]
                  [venantius/accountant "0.2.4"
                   :exclusions [org.clojure/tools.reader]]]
@@ -67,20 +67,20 @@
              :ring-handler site.handler/app}
 
   :profiles {:dev {:repl-options {:init-ns site.repl}
-                   :dependencies [[cider/piggieback "0.4.0"]
+                   :dependencies [[cider/piggieback "0.4.2"]
                                   [binaryage/devtools "0.9.10"]
                                   [orchestra "2019.02.06-1"]
-                                  [ring/ring-mock "0.3.2"]
+                                  [ring/ring-mock "0.4.0"]
                                   [ring/ring-devel "1.7.1"]
                                   [philoskim/debux "0.5.6"]
-                                  [prone "1.6.1"]
-                                  [figwheel-sidecar "0.5.18"]
-                                  [day8.re-frame/re-frame-10x "0.4.0"]
+                                  [prone "2019-07-08"]
+                                  [figwheel-sidecar "0.5.19"]
+                                  [day8.re-frame/re-frame-10x "0.4.4"]
                                   [nrepl "0.6.0"]
-                                  [pjstadig/humane-test-output "0.9.0"]]
+                                  [pjstadig/humane-test-output "0.10.0"]]
 
                    :source-paths ["env/dev"]
-                   :plugins [[lein-figwheel "0.5.18"]]
+                   :plugins [[lein-figwheel "0.5.19"]]
 
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
